5701b460fed0cd3ba9d7d94ac8b4fa071060e064
Oleg Skripnikov <oleg-skripnikov8@mail.ru>
2025-11-14 22:39:13 +0700

    add User model

--- config/settings.py
+++ config/settings.py
@@ -63,9 +63,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
