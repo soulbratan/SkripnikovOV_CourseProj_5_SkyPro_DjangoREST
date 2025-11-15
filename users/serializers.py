653a4be675c8ef38e8a3a0bd4fefc32e2b800564
Oleg Skripnikov <oleg-skripnikov8@mail.ru>
2025-11-15 21:02:36 +0700

    serializer for Users publicView

--- users/serializers.py
+++ users/serializers.py
@@ -5,9 +5,25 @@ from users.models import User
 class UserSerializer(ModelSerializer):
     """Сериализатор для пользователя"""
 
-    payments = PaymentSerializer(many=True, read_only=True, source="payment_set")
-
     class Meta:
         model = User
         fields = "__all__"
         extra_kwargs = {"password": {"write_only": True}}
+
+
+class PublicUserSerializer(ModelSerializer):
+    """Сериализатор для публичного просмотра профиля пользователя"""
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "first_name",
+            "phone",
+            "country",
+            "avatar",
+            "date_joined",
+            "last_login",
+        ]
+        read_only_fields = fields
