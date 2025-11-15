b211d775673931417bd34d0edc161e2b4e0d399c
Oleg Skripnikov <oleg-skripnikov8@mail.ru>
2025-11-15 21:04:06 +0700

    view for create User

--- users/views.py
+++ users/views.py
@@ -1,2 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """Создание пользователя"""
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
