43c0873c7e735f3a8de7a0de84ddbd80c37125b7
Oleg Skripnikov <oleg-skripnikov8@mail.ru>
2025-11-15 20:48:50 +0700

    add serializers.py, update users.urls.py

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from users.apps import UsersConfig
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    # users
+    # path("register/", UserCreateAPIView.as_view(), name="user-create"),
+    # path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user-retrieve"),
+    # path("", UserListAPIView.as_view(), name="user-list"),
+    # path("<int:pk>/update/", UserUpdateAPIView.as_view(), name="user-update"),
+    # path("<int:pk>/delete/", UserDestroyAPIView.as_view(), name="user-delete"),
+]
